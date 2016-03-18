623f1bed777b39e26f9caf5d403eb103f8e2cf3e
xiezuojia <xiezuojia1983@163.com>
2016-03-18 21:41:45 +0800

    rrrr

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(147, 50);
+            this.button1.Location = new System.Drawing.Point(39, 95);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
